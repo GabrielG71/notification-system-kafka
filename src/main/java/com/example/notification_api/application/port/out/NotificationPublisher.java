d148c761c2bd594a94f4bbdb3b9fdbbdbfd882d3
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-06 22:35:22 -0300

    feat(notification-api): add notification publisher output port

--- src/main/java/com/example/notification_api/application/port/out/NotificationPublisher.java
+++ src/main/java/com/example/notification_api/application/port/out/NotificationPublisher.java
@@ -1,3 +1,7 @@
-public class NotificationPublisher {
-    
+package com.example.notification_api.application.port.out;
+
+import com.example.notification_api.domain.Notification;
+
+public interface NotificationPublisher {
+    void publish(Notification notification);
 }
